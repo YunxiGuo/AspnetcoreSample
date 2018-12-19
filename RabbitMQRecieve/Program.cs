f85d41cb1fc9226e47cb865616decf7c17c90352
郭云喜 <guoyunxi1198@163.com>
2018-12-19 17:28:26 +0800

    添加RabbitMQ的基础示例

---
+++ RabbitMQRecieve/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQRecieve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "hello",
+                        durable: false,
+                        exclusive: false, 
+                        autoDelete: false,
+                        arguments: null);
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += Consumer_Received;
+                    channel.BasicConsume(queue: "hello",
+                        autoAck: true,
+                        consumer: consumer);
+                    Console.WriteLine("Press [enter] to exit");
+
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body;
+            var msg = Encoding.UTF8.GetString(body);
+            Console.WriteLine($"[x] Recieve {msg}");
+        }
+    }
+}
---
+++ RabbitMQSend/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMQSend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+                    string msg = "Hello RabbitMQ";
+                    var body = Encoding.UTF8.GetBytes(msg);
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: "hello",
+                        mandatory: false,
+                        basicProperties: null,
+                        body: body);
+                    Console.WriteLine($"[x] Send {msg}");
+                }
+            }
+            Console.WriteLine("Press [enter] to exit");
+
+            Console.ReadLine();
+        }
+    }
+}
